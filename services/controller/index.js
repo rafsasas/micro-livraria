df8047f824e72047fbc756f36cc2419dc4c43e75
rafsasas <rafael.sandim2@gmail.com>
2025-11-30 23:15:57 -0400

    Tarefa prática #1 - Microservices

--- services/controller/index.js
+++ services/controller/index.js
@@ -10,13 +10,13 @@ app.use(express.json());
 /**
  * Retorna a lista de produtos da loja via InventoryService
  */
-app.get('/products', (req, res, next) => {
-    inventory.SearchAllProducts(null, (err, data) => {
+app.get('/product/:id', (req, res, next) => {
+    inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
         if (err) {
             console.error(err);
             res.status(500).send({ error: 'something failed :(' });
         } else {
-            res.json(data.products);
+            res.json(product);
         }
     });
 });
--- services/inventory/index.js
+++ services/inventory/index.js
@@ -15,10 +15,11 @@ const server = new grpc.Server();
 
 // implementa os métodos do InventoryService
 server.addService(inventoryProto.InventoryService.service, {
-    searchAllProducts: (_, callback) => {
-        callback(null, {
-            products: products,
-        });
+        SearchProductByID: (payload, callback) => {
+        callback(
+            null,
+            products.find((product) => product.id == payload.request.id)
+        );
     },
 });
 
